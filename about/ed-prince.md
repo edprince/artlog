b6a65e886aa9aec70230d50601bfb73b2f80b054
ed <edward_prince@hotmail.com>
2023-08-28 20:35:17 +0100

    added nav and status to readme

---
+++ about/ed-prince.md
@@ -0,0 +1,5 @@
+---
+title: Ed Prince
+thumbnail: /images/uploads/pxl_20221124_140749365.jpeg
+---
+Hi, I'm Ed! I'm an illustrator based in London. I love creating works that capture the spirit of characters and the relationships between them. I'm naturally drawn to those little moments of love, friendship, and camaraderie.
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
 import NavLink from "./NavLink";
+import NameIcon from "../../public/name.svg";
+import Image from "next/image";
 
 export default function Nav() {
     return (
-        <nav className="p-8 md:px-24 text-right space-x-8">
-            <NavLink url="/" label="Portfolio" />
-            <NavLink url="/thumbnails" label="Thumbnails" />
-            <NavLink url="/about" label="About" />
-        </nav>
+        <div className="px-8 md:px-24 flex flex-row">
+            <Link href="/">
+                <Image src={NameIcon} alt="Name" />
+            </Link>
+            <div className="flex-grow"></div>
+            <nav className="space-x-8 flex align-center items-center">
+                <NavLink url="/" label="Portfolio" />
+                <NavLink url="/thumbnails" label="Thumbnails" />
+                <NavLink url="/about" label="About" />
+            </nav>
+        </div>
     )
 }
